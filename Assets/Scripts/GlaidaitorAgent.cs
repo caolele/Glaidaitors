0211168ac6c013241d0e6bcf1d8e88de7725846d
Daniel Hernandez <danielhp95@gmail.com>
2018-05-31 23:08:03 +0300

    Added initial implementation of glaidaitoragent.

--- Assets/Scripts/GlaidaitorAgent.cs
+++ Assets/Scripts/GlaidaitorAgent.cs
@@ -4,11 +4,16 @@ using UnityEngine;
 
 public class GlaidaitorAgent : Agent
 {
-    [Header("Specific to GLAIDAITORS")]
-    public GameObject ball;
+
+    private Vector3 arenaCenterPosition;
+
+    private GameObject agent;
+    [HideInInspector]
+    private GlaidaitorAcademy academy; 
 
     public override void InitializeAgent()
     {
+        this.arenaCenterPosition = Vector3.zero;
     }
 
     public override void CollectObservations()
@@ -17,45 +22,56 @@ public class GlaidaitorAgent : Agent
 
     }
 
-    public override void AgentAction(float[] vectorAction, string textAction)
-    {
-        if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous)
-        {
-            float action_z = 2f * Mathf.Clamp(vectorAction[0], -1f, 1f);
-            if ((gameObject.transform.rotation.z < 0.25f && action_z > 0f) ||
-                (gameObject.transform.rotation.z > -0.25f && action_z < 0f))
-            {
-                gameObject.transform.Rotate(new Vector3(0, 0, 1), action_z);
-            }
-            float action_x = 2f * Mathf.Clamp(vectorAction[1], -1f, 1f);
-            if ((gameObject.transform.rotation.x < 0.25f && action_x > 0f) ||
-                (gameObject.transform.rotation.x > -0.25f && action_x < 0f))
-            {
-                gameObject.transform.Rotate(new Vector3(1, 0, 0), action_x);
-            }
 
-            SetReward(0.1f);
-
-        }
-        if ((ball.transform.position.y - gameObject.transform.position.y) < -2f ||
-            Mathf.Abs(ball.transform.position.x - gameObject.transform.position.x) > 3f ||
-            Mathf.Abs(ball.transform.position.z - gameObject.transform.position.z) > 3f)
-        {
-            Done();
-            SetReward(-1f);
-        }
+    private void HandleMovement(float[] action) {
 
 
     }
 
+    public override void AgentAction(float[] vectorAction, string textAction)
+    {
+        if (brain.brainParameters.vectorActionSpaceType == SpaceType.continuous)
+        {
+           HandleMovement(vectorAction);
+           checkForDeath();
+        } else {
+            print("STATE SPACE SHOULD BE CONTINUOUS");
+        }
+
+    }
+
+    private void checkForDeath() {
+        float distanceFromArenaCenter = Vector3.Distance(this.transform.position, this.arenaCenterPosition);
+
+        if (distanceFromArenaCenter > academy.arenaRadius) {
+            Done();
+            SetReward(-academy.offTheRingReward);
+        }
+    }
+
     public override void AgentReset()
     {
-        //gameObject.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
-        //gameObject.transform.Rotate(new Vector3(1, 0, 0), Random.Range(-10f, 10f));
-        //gameObject.transform.Rotate(new Vector3(0, 0, 1), Random.Range(-10f, 10f));
-        //ball.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
-        //ball.transform.position = new Vector3(Random.Range(-1.5f, 1.5f), 4f, Random.Range(-1.5f, 1.5f)) + gameObject.transform.position;
+        Vector3 newPosition = getRandomNewPosition();
+        Quaternion newRotation = getRandomNewQuaternionInXZPlane();
+    
+        agent.transform.position = newPosition;
+        agent.transform.rotation = newRotation;
+        agent.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
 
+        // gameObject.transform.rotation = new Quaternion(0f, 0f, 0f, 0f);
+        // ball.GetComponent<Rigidbody>().velocity = new Vector3(0f, 0f, 0f);
+        // ball.transform.position = new Vector3(Random.Range(-1.5f, 1.5f), 4f, Random.Range(-1.5f, 1.5f)) + gameObject.transform.position;
+    }
+
+    private Vector3 getRandomNewPosition() {
+        float offsetFromCenter = Random.Range(0f, academy.arenaRadius);
+        float radians = Random.Range(0f, 360f) * Mathf.Deg2Rad;
+        Vector3 newCoord = new Vector3(Mathf.Sin(radians), transform.position.y, Mathf.Cos(radians));
+        return offsetFromCenter * newCoord; 
+    }
+
+    private Quaternion getRandomNewQuaternionInXZPlane() {
+        return Quaternion.Euler(0, Random.Range(0f, 360f), 0);
     }
 
 }
