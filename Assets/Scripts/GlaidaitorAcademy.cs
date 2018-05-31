3d9a795fb70da1915881ca71ff0b84548018dec1
Sami Purmonen <sami.purmonen@king.com>
2018-05-31 18:52:00 +0200

    Added agent

---
+++ Assets/Scripts/GlaidaitorAcademy.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GlaidaitorAcademy : Academy
+{
+
+    public float knockBackForce;
+    public float hitReward;
+    public float offTheRingReward;
+
+    public override void AcademyReset()
+    {
+
+    }
+
+    public override void AcademyStep()
+    {
+
+    }
+}
